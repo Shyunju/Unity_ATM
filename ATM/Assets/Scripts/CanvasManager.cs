53d1434bee48b05e9df29c8ae51330275e20959b
Shyunju <hyunju9908@naver.com>
2023-09-22 10:47:25 +0900

    [DELETE] 불필요 요소 삭제

--- ATM/Assets/Scripts/CanvasManager.cs
+++ ATM/Assets/Scripts/CanvasManager.cs
@@ -6,10 +6,7 @@ public class CanvasManager : MonoBehaviour
 {
     [SerializeField] private GameObject open;
     [SerializeField] private GameObject close;
-    void Start()
-    {
 
-    }
     public void OpenCanvas()
     {
         open.SetActive(true);
